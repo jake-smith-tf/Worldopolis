944abf8ebc572701e61387352f691baefafe0e0b
Jake Smith <jakesmith@Macbook-Pro.local>
2018-09-20 15:32:35 +0100

    Initial Commit

---
+++ Worldopolis/Joystick.swift
@@ -0,0 +1,9 @@
+//
+//  Joystick.swift
+//  Worldopolis
+//
+//  Created by Jake Smith on 03/08/2018.
+//  Copyright © 2018 Nebultek. All rights reserved.
+//
+
+import Foundation
---
+++ Worldopolis/Rope.swift
@@ -0,0 +1,9 @@
+//
+//  Rope.swift
+//  Worldopolis
+//
+//  Created by Jake Smith on 04/08/2018.
+//  Copyright © 2018 Nebultek. All rights reserved.
+//
+
+import Foundation
